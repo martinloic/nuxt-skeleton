77bafe476279c3940ced957801fe4cd81110d01a
Loïc MARTIN <martin.loic.prod@gmail.com>
2025-02-10 10:27:43 +0100

    chore(lint): update ESLint config for Vue.js and Nuxt compatibility

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,13 @@ import withNuxt from './.nuxt/eslint.config.mjs';
 
 export default withNuxt({
   rules: {
+    // Vuejs
+    'vue/first-attribute-linebreak': ['error', {
+      singleline: 'beside',
+      multiline: 'below'
+    }],
+    'vue/multi-word-component-names': 0,
+    // Nuxt
     '@stylistic/semi': ['error', 'always'],
     '@stylistic/comma-dangle': ['error', 'never'],
     '@typescript-eslint/no-unused-vars': [
