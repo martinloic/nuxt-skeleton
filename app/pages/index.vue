a9941e0ecf8bc43aca690538209b421a863e64e1
Loïc MARTIN <martin.loic.prod@gmail.com>
2025-07-19 16:00:11 +0200

    feat(page): implement user fetching functionality in admin layout

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -6,8 +6,36 @@
     <h1 class="text-4xl">
       Hello from Nuxt3 Skeleton
     </h1>
+    <div v-if="users">
+      {{ users }}
+    </div>
+    <UButton
+      class="mt-4"
+      label="Fetch Users"
+      @click="fetchUsers"
+    />
   </div>
 </template>
 
 <script lang="ts" setup>
+definePageMeta({
+  layout: 'admin',
+  middleware: 'auth'
+});
+const users = ref<{ id: string, name: string, email: string }[] | null>(null);
+
+async function fetchUsers() {
+  // Fetch users from the API
+  return $fetch('/api/users', {
+    method: 'GET'
+  }).then((data) => {
+    users.value = data;
+  }).catch((error) => {
+    console.error('Error fetching users:', error);
+  });
+}
+
+onMounted(async () => {
+  await fetchUsers();
+});
 </script>
