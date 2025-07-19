b0e58f3fc3e46b28c709868d13a713c8cbdf415b
Loïc MARTIN <martin.loic.prod@gmail.com>
2025-07-19 20:53:11 +0200

    feat(config): enhance UI configuration with dropdown, select, and button styles

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,2 +1,37 @@
 export default defineAppConfig ({
+  ui: {
+    colors: {
+      primary: 'gray'
+    },
+    dropdownMenu: {
+      button: {
+        slots: {
+          base: 'cursor-pointer'
+        }
+      },
+      slots: {
+        item: 'cursor-pointer'
+      }
+    },
+    select: {
+      slots: {
+        item: 'cursor-pointer'
+      }
+    },
+    selectMenu: {
+      slots: {
+        item: 'cursor-pointer'
+      }
+    },
+    button: {
+      slots: {
+        base: 'cursor-pointer'
+      }
+    },
+    navigationMenu: {
+      slots: {
+        list: 'cursor-pointer'
+      }
+    }
+  }
 });
