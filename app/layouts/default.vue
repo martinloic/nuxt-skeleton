b170262fa7f681eaca14bca03437b3d5aa7eacf2
Loïc MARTIN <martin.loic.prod@gmail.com>
2025-07-19 20:53:25 +0200

    feat(page): enhance login page layout and styling for improved user experience

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="flex flex-col items-center justify-center bg-[#F6F8FA] md:h-screen md:w-screen">
     <slot />
   </div>
 </template>
--- app/pages/login.vue
+++ app/pages/login.vue
@@ -1,32 +1,52 @@
 <template>
-  <div>
-    <UForm
-      :schema="loginSchema"
-      :state="state"
-      class="space-y-4"
-      @submit="onSubmit"
-    >
-      <UFormField
-        label="Email"
-        name="email"
+  <div class="flex flex-col-reverse p-6 md:grid md:h-screen md:w-screen md:grid-cols-7">
+    <div class="relative col-span-4 mt-16 hidden flex-col rounded-[32px] border border-gray-200 bg-white md:mt-0 md:flex md:overflow-hidden" />
+    <div class="col-span-3 flex h-screen flex-col items-center justify-center md:h-auto">
+      <UForm
+        :schema="loginSchema"
+        :state="state"
+        class="space-y-4 bg-white py-8 px-10 rounded-xl"
+        @submit="onSubmit"
       >
-        <UInput v-model="state.email" />
-      </UFormField>
+        <div class="space-y-1">
+          <h1 class="font-extrabold text-center text-lg">
+            Sign in to your account
+          </h1>
+          <p class="text-center text-gray-500 text-sm">
+            Welcome back! Please sign in to continue
+          </p>
+        </div>
+        <UFormField
+          label="Email"
+          name="email"
+        >
+          <UInput
+            v-model="state.email"
+            type="text"
+            class="w-full"
+          />
+        </UFormField>
 
-      <UFormField
-        label="Password"
-        name="password"
-      >
-        <UInput
-          v-model="state.password"
-          type="password"
-        />
-      </UFormField>
+        <UFormField
+          label="Password"
+          name="password"
+        >
+          <UInput
+            v-model="state.password"
+            type="password"
+            class="w-full"
+          />
+        </UFormField>
 
-      <UButton type="submit">
-        Submit
-      </UButton>
-    </UForm>
+        <UButton
+          type="submit"
+          class="flex ml-auto w-full justify-center"
+          icon=""
+        >
+          Continue
+        </UButton>
+      </UForm>
+    </div>
   </div>
 </template>
 
