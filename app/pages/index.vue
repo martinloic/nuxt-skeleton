e041ebf9ae251746033bca936248d715371d957f
Loïc MARTIN <martin.loic.prod@gmail.com>
2025-07-19 16:14:24 +0200

    refactor(page): streamline user fetching by removing button and using useFetch directly

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -9,11 +9,6 @@
     <div v-if="users">
       {{ users }}
     </div>
-    <UButton
-      class="mt-4"
-      label="Fetch Users"
-      @click="fetchUsers"
-    />
   </div>
 </template>
 
@@ -22,20 +17,8 @@ definePageMeta({
   layout: 'admin',
   middleware: 'auth'
 });
-const users = ref<{ id: string, name: string, email: string }[] | null>(null);
 
-async function fetchUsers() {
-  // Fetch users from the API
-  return $fetch('/api/users', {
-    method: 'GET'
-  }).then((data) => {
-    users.value = data;
-  }).catch((error) => {
-    console.error('Error fetching users:', error);
-  });
-}
-
-onMounted(async () => {
-  await fetchUsers();
+const users = await useFetch('/api/users', {
+  method: 'GET'
 });
 </script>
