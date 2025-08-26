0331b17f6774458c98829806b400366fca54675b
Loïc MARTIN <martin.loic.prod@gmail.com>
2025-08-26 09:34:33 +0200

    chore: update dependencies and devDependencies in package.json
    - Bump versions for @nuxt/image, @nuxt/ui, @vueuse/core, @vueuse/nuxt, @prisma/client, nuxt, nuxt-auth-utils, tailwindcss, zod, and prisma.
    - Update devDependencies for @nuxt/eslint, eslint, and typescript.
    
    fix: comment out console log in demo middleware
    
    - Commented out the console log statement in server/middleware/demo.ts to reduce noise in logs.
    
    feat: add PASSWORD_REGEX utility
    
    - Introduced a new utility for password validation in shared/utils/auth.ts.

--- server/middleware/demo.ts
+++ server/middleware/demo.ts
@@ -1,5 +1,5 @@
 import type { H3Event } from 'h3';
 
 export default defineEventHandler((event: H3Event) => {
-  console.log('hello from server middleware', event.__is_event__);
+  // console.log('hello from server middleware', event.__is_event__);
 });
